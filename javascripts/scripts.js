881a37e2c405be67d77c162a03d0bdfb0764fea4
Ilia Puzyrev <ipzrev@gmail.com>
2026-03-11 17:40:55 +0300

    14

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,15 +1,34 @@
+const button = document.getElementById("enable");
+
+button.addEventListener("click", async () => {
+  if (
+    typeof DeviceOrientationEvent !== "undefined" &&
+    typeof DeviceOrientationEvent.requestPermission === "function"
+  ) {
+    const permission = await DeviceOrientationEvent.requestPermission();
+
+    if (permission === "granted") {
+      startParallax();
+      button.style.display = "none";
+    }
+  } else {
+    startParallax();
+    button.style.display = "none";
+  }
+});
+
 function startParallax() {
   const layers = document.querySelectorAll(".layer");
 
   window.addEventListener("deviceorientation", (event) => {
-    const x = event.gamma; // наклон влево/вправо
-    const y = event.beta; // наклон вперед/назад
+    const gamma = event.gamma; // влево вправо
+    const beta = event.beta; // вперед назад
 
     layers.forEach((layer) => {
-      const depth = layer.getAttribute("data-depth");
+      const depth = layer.dataset.depth;
 
-      const moveX = x * depth * 0.2;
-      const moveY = y * depth * 0.2;
+      const moveX = gamma * depth * 0.5;
+      const moveY = beta * depth * 0.5;
 
       layer.style.transform = `translate(-50%, -50%) translate(${moveX}px, ${moveY}px)`;
     });
