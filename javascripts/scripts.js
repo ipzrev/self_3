aad36c47b95de8c9ccca0699f6de66f561ad0a2a
Ilia Puzyrev <ipzrev@gmail.com>
2026-03-11 17:50:15 +0300

    15

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,6 +1,8 @@
 const button = document.getElementById("enable");
 
-button.addEventListener("click", async () => {
+button.addEventListener("click", requestGyro);
+
+async function requestGyro() {
   if (
     typeof DeviceOrientationEvent !== "undefined" &&
     typeof DeviceOrientationEvent.requestPermission === "function"
@@ -15,20 +17,20 @@ button.addEventListener("click", async () => {
     startParallax();
     button.style.display = "none";
   }
-});
+}
 
 function startParallax() {
   const layers = document.querySelectorAll(".layer");
 
   window.addEventListener("deviceorientation", (event) => {
-    const gamma = event.gamma; // влево вправо
-    const beta = event.beta; // вперед назад
+    const gamma = event.gamma || 0;
+    const beta = event.beta || 0;
 
     layers.forEach((layer) => {
       const depth = layer.dataset.depth;
 
-      const moveX = gamma * depth * 0.5;
-      const moveY = beta * depth * 0.5;
+      const moveX = gamma * depth * 0.3;
+      const moveY = beta * depth * 0.3;
 
       layer.style.transform = `translate(-50%, -50%) translate(${moveX}px, ${moveY}px)`;
     });
